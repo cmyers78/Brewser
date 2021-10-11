f9de89f18782a91185980b0f54c657d8190dd2d2
Chris Myers <cmyers78@gmail.com>
2021-10-11 09:23:27 -0400

    Initial commit. Added swftlint to new project for testing.

---
+++ Brewser/Coordinator/AppCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  AppCoordinator.swift
+//  Brewser
+//
+//  Created by Chris Myers on 10/8/21.
+//
+
+import Foundation
+import UIKit
+
+class AppCoordinator: Coordinator {
+	let window: UIWindow
+	let rootViewController: UINavigationController
+	
+	init(_ window: UIWindow) {
+		self.window = window
+		rootViewController = UINavigationController()
+		
+		let mainVC = MainViewController()
+		rootViewController.pushViewController(mainVC, animated: true)
+	}
+	
+	func start() {
+		window.rootViewController = rootViewController
+		window.makeKeyAndVisible()
+	}
+}
---
+++ Brewser/Coordinator/Coordinator.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinator.swift
+//  Brewser
+//
+//  Created by Chris Myers on 10/8/21.
+//
+
+import Foundation
+
+protocol Coordinator {
+	func start()
+}
